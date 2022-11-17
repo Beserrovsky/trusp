7730c67c991117c44328b5a8235ad629ea47aea8
Beserrovsky <felipebeserra25@gmail.com>
2022-11-17 19:39:17 -0300

    Ldr added to colections

--- proj/server/index.js
+++ proj/server/index.js
@@ -1,23 +1,31 @@
 const { MongoClient } = require('mongodb');
 const mqtt = require('async-mqtt');
 
+// Topic for db warnings
+const WRN_TOPIC = 'TRUSP-db';
+
+// Database Name
+const DB_NAME = 'TRUSP';
+
 const db_uri =
-  'mongodb://localhost:27017';
+  'mongodb://localhost:27017/' + DB_NAME;
 
 const mqtt_uri = 
   'mqtt://broker.hivemq.com';
 
-// Topic for db warnings
-const WRN_TOPIC = 'TRUSP-db';
-
-const SUBS = {'TRUSP-led': {qos: 0}, 'TRUSP-dht': {qos: 1}};
+const SUBS = {'TRUSP-itn': {qos: 0}, 'TRUSP-dht': {qos: 1}};
 
 async function db_conn() {
   try {
     await db_client.connect();
 
-    await db_client.db('admin').command({ ping: 1 });
+    let dbo = db_client.db(DB_NAME);
+
+    dbo.command({ ping: 1 });
+
     console.log('Connected successfully to mongodb server!');
+    
+    return dbo;
   } finally { }
 }
 
@@ -31,8 +39,28 @@ async function mqtt_conn() {
   } finally { }
 }
 
-async function handleMsg(topic, msg) {
-  console.log(msg.toString());
+async function handleMsg(topic, msg, dbo) {
+  msg = msg.toString();
+
+  console.log(`Message received on "${topic}": ${msg.toString()}`);
+
+  // Save to Database
+  switch(topic) {
+    case 'TRUSP-itn':
+      const itn = dbo.collection('itn');  
+    
+      const doc = {
+        itensity: Number(msg),
+        timestamp: Date. now()
+      }  
+
+      const result = await itn.insertOne(doc);
+      console.log(
+        `A itn document was inserted with the _id: ${result.insertedId}`,
+      );
+      break;
+  }
+
 }
 
 const db_client = new MongoClient(db_uri);
@@ -40,13 +68,13 @@ const db_client = new MongoClient(db_uri);
 async function main() {
   
   // Connect to MongoDB
-  await db_conn().catch(console.dir);
+  let dbo = await db_conn().catch(console.dir);
 
   // Connect to broker
   const client = await mqtt_conn().catch(console.dir);
 
   // Set cb
-  await client.on('message', handleMsg);
+  await client.on('message', (topic, msg) => handleMsg(topic, msg, dbo));
 
   // Subscribes to topics
   await client.subscribe(SUBS, (err) => err? console.error : null);
